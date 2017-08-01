413633b05c2436b3774e7f0a064f2664c080e592
Mithun <mithun-prasad@users.noreply.github.com>
2017-08-01 13:59:23 +1200

    InBytesBot

---
+++ Student-Resources/Labs/CSharp/TennisBot/InBytesBot/EchoDialog.cs
@@ -0,0 +1,14 @@
+﻿[Serializable]
+public class EchoDialog : IDialog<object>
+{
+    public async Task StartAsync(IDialogContext conTtext)
+    {
+        context.Wait(MessageReceivedAsync);
+    }
+    public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+    {
+        var message = await argument;
+        await context.PostAsync("You said: " + message.Text);
+        context.Wait(MessageReceivedAsync);
+    }
+}
