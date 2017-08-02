25453717aeafd02eff7fb6270cdd4d1f6ac8e030
Mithun <mithun-prasad@users.noreply.github.com>
2017-08-02 15:47:09 +1200

    sentiment bots

---
+++ Student-Resources/Labs/CSharp/SentimentBot/TextSentiBot/Controllers/MessagesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace TextSentiBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            if (activity == null || activity.GetActivityType() != ActivityTypes.Message)
+            {
+                //add code to handle errors, or non-messaging activities
+            }
+
+            const string apiKey = "2562e5c336844e37a93db20fc0a00367";
+            string queryUri = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            BatchInput sentimentInput = new BatchInput();
+
+            sentimentInput.documents = new List<DocumentInput>();
+            sentimentInput.documents.Add(new DocumentInput()
+            {
+                id = 1,
+                text = activity.Text
+            });
+
+            var sentimentJsonInput = JsonConvert.SerializeObject(sentimentInput);
+            byte[] byteData = Encoding.UTF8.GetBytes(sentimentJsonInput);
+            var content = new ByteArrayContent(byteData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var sentimentPost = await client.PostAsync(queryUri, content);
+            var sentimentRawResponse = await sentimentPost.Content.ReadAsStringAsync();
+            var sentimentJsonResponse = JsonConvert.DeserializeObject<BatchResult>(sentimentRawResponse);
+            double sentimentScore = sentimentJsonResponse.documents[0].score;
+
+            var replyMessage = activity.CreateReply();
+            replyMessage.Recipient = activity.From;
+            replyMessage.Type = ActivityTypes.Message;
+
+            if (sentimentScore > 0.9)
+            {
+                replyMessage.Text = $"This appears quite positive to me.";
+            }
+            else if (sentimentScore < 0.1)
+            {
+                replyMessage.Text = $"This appears quite negative to me.";
+            }
+            else
+            {
+                replyMessage.Text = $"I can not decipher the sentiment here.  Please try again or add more information.";
+            }
+
+            await connector.Conversations.ReplyToActivityAsync(replyMessage);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Student-Resources/Labs/CSharp/SentimentBot/TextSentiBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TextSentiBot.WebApiApplication" Language="C#" %>
